405600c4e6c8be883982b890512f4cee2d733e29
vivianbarclay <vivian.m.barclay@gmail.com>
2025-02-12 05:40:29 +0000

    omni calc rails vb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,58 @@
 class ApplicationController < ActionController::Base
-  skip_forgery_protection
+  def home
+  end
+
+  def square_form
+  end
+
+  def square_results
+    @number = params[:number]
+    @square = @number.to_f**2
+  end
+
+  def square_root_form
+  end
+
+  def square_root_results
+    @number = params[:number]
+    @square_root = @number.to_f**0.5
+  end
+
+  def payment_form
+  end
+
+  def payment_results
+    @apr = params[:apr].to_f
+    @years = params[:years].to_i
+    @principal = params[:principal].to_f
+
+    
+    r = (@apr/100)/12
+    n = @years*12
+
+  if r == 0
+    payment = (@principal/n).round(2)
+  else
+    payment = ((r*@principal)/(1-((1+r)**-n))).round(2)
+  end
+  
+  @formatted_apr = "#{format('%.4f', @apr)}%"
+  @formatted_principal = "$#{format('%.2f', @principal).to_s.gsub(/(\d)(?=(\d{3})+\.)/, '\\1,')}"
+  @formatted_payment = "$#{format('%.2f', payment).to_s.gsub(/(\d)(?=(\d{3})+\.)/, '\\1,')}" 
+
+  end
+
+  def random_form
+  end
+
+  def random_results
+    @minimum = params[:minimum].to_f
+    @maximum = params[:maximum].to_f
+
+    @random = rand(@minimum..@maximum)
+
+  end
+  
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  
+  root 'application#square_form'  # Home page
+
+  # Square calculator routes
+  get '/square/new', to: 'application#square_form', as: 'new_square'
+  get '/square/results', to: 'application#square_results', as: 'square_results'
+  get '/square_root/new', to: 'application#square_root_form', as: 'new_square_root'
+  get '/square_root/results', to: 'application#square_root_results', as: 'square_root_results'
+  get '/payment/new', to: 'application#payment_form', as: 'new_payment'
+  get '/payment/results', to: 'application#payment_results', as: 'payment_results'
+  get '/random/new', to: 'application#random_form', as: 'new_random'
+  get '/random/results', to: 'application#random_results', as: 'random_results'
 end
